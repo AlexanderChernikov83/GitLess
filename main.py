199c29e3b23c933933c78a929031187a77305a57
AlexanderChernikov83 <Cahe4ka2@ya.ru>
2025-08-29 20:58:56 +0300

    update
    версия с циклом while

--- main.py
+++ main.py
@@ -65,3 +65,50 @@ def calculator():
 
 print(calculator())
 
+
+
+import math
+
+def calculator():
+    print("Простой калькулятор")
+    print("Доступные операции: +, -, *, /, sqrt")
+    print("Для выхода напишите: exit")
+
+    while True:
+        operator = input("\nВведите операцию (+, -, *, /, sqrt, exit): ")
+
+        if operator == "exit":
+            print("Выход из программы...")
+            break
+
+        if operator == "sqrt":
+            num = float(input("Введите число: "))
+            if num >= 0:
+                result = math.sqrt(num)
+                print(f"Результат: {result}")
+            else:
+                print("Ошибка: нельзя извлечь корень из отрицательного числа!")
+        else:
+            num1 = float(input("Введите первое число: "))
+            num2 = float(input("Введите второе число: "))
+
+            if operator == "+":
+                result = num1 + num2
+            elif operator == "-":
+                result = num1 - num2
+            elif operator == "*":
+                result = num1 * num2
+            elif operator == "/":
+                if num2 != 0:
+                    result = num1 / num2
+                else:
+                    print("Ошибка: деление на ноль!")
+                    continue
+            else:
+                print("Ошибка: неверная операция!")
+                continue
+
+            print(f"Результат: {result}")
+
+calculator()
+
