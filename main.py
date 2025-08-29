715553e5cdbd82eaba0c61020b347ffd839d285c
AlexanderChernikov83 <Cahe4ka2@ya.ru>
2025-08-29 20:29:51 +0300

    Add sqrt
    добавление вычисления квадратного корня

--- main.py
+++ main.py
@@ -24,3 +24,44 @@ def calculator():
 
 
 print(calculator())
+
+
+
+
+import math
+
+def calculator():
+    print("Простой калькулятор")
+    print("Доступные операции: +, -, *, /, sqrt")
+
+    operator = input("Введите операцию (+, -, *, /, sqrt): ")
+
+    if operator == "sqrt":
+        num = float(input("Введите число: "))
+        if num >= 0:
+            result = math.sqrt(num)
+        else:
+            return "Ошибка: нельзя извлечь корень из отрицательного числа!"
+    else:
+        num1 = float(input("Введите первое число: "))
+        num2 = float(input("Введите второе число: "))
+
+        if operator == "+":
+            result = num1 + num2
+        elif operator == "-":
+            result = num1 - num2
+        elif operator == "*":
+            result = num1 * num2
+        elif operator == "/":
+            if num2 != 0:
+                result = num1 / num2
+            else:
+                return "Ошибка: деление на ноль!"
+        else:
+            return "Ошибка: неверная операция!"
+
+    return f"Результат: {result}"
+
+
+print(calculator())
+
